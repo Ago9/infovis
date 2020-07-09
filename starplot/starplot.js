60714e7aaf289802fa0e7503ebb5a36a52998a96
Agostino G. Strati <44027309+Ago9@users.noreply.github.com>
2020-07-09 18:15:19 +0200

    update

--- starplot/starplot.js
+++ starplot/starplot.js
@@ -36,16 +36,14 @@ for(var j=0; j<=level; j++){
         .data(features)
         .enter()
         .append("svg:line")
-        .attr("x1", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).x;})
-        .attr("y1", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).y;})
-        .attr("x2", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * (i+1) / n_axis), j*2).x;})
-        .attr("y2", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * (i+1) / n_axis), j*2).y;})
+        .attr("x1", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).x;})
+        .attr("y1", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).y;})
+        .attr("x2", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * (i+1) / n_axis), j*2).x;})
+        .attr("y2", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * (i+1) / n_axis), j*2).y;})
         .attr("stroke", "#a3a5a8")
         .attr("stroke-width", "2.5px")
         .attr("stroke-opacity","0.9")
-        .on("click",function(d,i){
-            changeAxes(data, i, i+1);
-        })           
+        
 }
 
 // plot the test
@@ -55,13 +53,13 @@ for(var j=0; j<=level; j++){
         .data([1])
         .enter()
         .append("svg:text")
-        .attr("x", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).x + 5;})
-        .attr("y", function(d,i){ return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).y - 5;})
+        .attr("x", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).x + 5;})
+        .attr("y", function(d,i){ return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), j*2).y - 5;})
         .text(j*2)
 }
 
 
-function angleToCoordiante(angle, value){
+function angleToCoordinate(angle, value){
     var x = Math.cos(angle) * radialScale(value);
     var y = Math.sin(angle) * radialScale(value);
     return { "x": 300 + x, "y": 300 - y };
@@ -81,10 +79,10 @@ axis.append("line")
      .attr("class", "line")
      .attr("x1", 300)
      .attr("y1", 300)
-     .attr("x2", function(d,i) { return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), 10).x;})
-     .attr("y2", function(d,i) { return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), 10).y;})
+     .attr("x2", function(d,i) { return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), 10).x;})
+     .attr("y2", function(d,i) { return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), 10).y;})
      .attr("stroke", "#abb3ad")
-     .attr("stroke-width", "3px")
+     .attr("stroke-width", "2px")
      .attr("stroke-opacity","2")
      .on("click", function(d,i){
          updateData(data, i);
@@ -94,8 +92,8 @@ axis.append("line")
 axis.append("text")
      .attr("class", function(d,i){ return "label" + i; })
      .text(function(d) { return d; })
-     .attr("x", function(d,i) { return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), 11).x;})
-     .attr("y", function(d,i) { return angleToCoordiante((Math.PI/2) + (2 * Math.PI * i / n_axis), 11).y;});
+     .attr("x", function(d,i) { return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), 11).x;})
+     .attr("y", function(d,i) { return angleToCoordinate((Math.PI/2) + (2 * Math.PI * i / n_axis), 11).y;});
         
 
 // line
@@ -109,7 +107,7 @@ function getPathCoordinates(data_point){
     for (var i = 0; i<n_axis; i++){
         var ft_name = data_point[i];
         var angle = (Math.PI/2) + (2* Math.PI * i / n_axis);
-        coordinate.push(angleToCoordiante(angle, ft_name.value));
+        coordinate.push(angleToCoordinate(angle, ft_name.value));
     }
     return coordinate;
 }
@@ -147,6 +145,120 @@ for (var i = 0; i < data.length; i ++){
         });
 }
 
+var triangolo1 = [];
+triangolo1.push({ "x": 300, "y": 300 });
+point = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 0 / n_axis), 10)
+point1 = angleToCoordinate((Math.PI/2) + (2 * Math.PI * (1) / n_axis), 10)
+point.x = point.x -4
+point.y = point.y +4
+point1.x = point1.x +4
+point1.y = point1.y -4
+triangolo1.push(point);
+triangolo1.push(point1);
+triangolo1.push({ "x": 300, "y": 300 });
+
+t1 = g.append("path")
+        .datum(triangolo1)
+        .attr('class', "triangolo" + i)
+        .attr("d", line)
+        .attr("visibility", "Hidden")
+        .attr("pointer-events", "fill")
+        .on("click", function(){
+           i = d3.select(this).attr("stroke-dashoffset");
+           changeAxes(0);
+        })
+
+var triangolo2 = [];
+triangolo2.push({ "x": 300, "y": 300 });
+point = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 1 / n_axis), 10)
+point1 = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 2 / n_axis), 10)
+point.x = point.x -4
+point.y = point.y +4
+point1.x = point1.x -4
+point1.y = point1.y +4
+triangolo2.push(point);
+triangolo2.push(point1);
+triangolo2.push({ "x": 300, "y": 300 });
+
+t2 = g.append("path")
+        .datum(triangolo2)
+        .attr('class', "triangolo" + i)
+        .attr("d", line)
+        .attr("visibility", "Hidden")
+        .attr("pointer-events", "fill")
+        .on("click", function(){
+           i = d3.select(this).attr("stroke-dashoffset");
+           changeAxes(1);
+        })
+
+var triangolo3 = [];
+triangolo3.push({ "x": 300, "y": 300 });
+point = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 2 / n_axis), 10)
+point1 = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 3 / n_axis), 10)
+point.x = point.x +4
+point.y = point.y -4
+point1.x = point1.x -4
+point1.y = point1.y +4
+triangolo3.push(point);
+triangolo3.push(point1);
+triangolo3.push({ "x": 300, "y": 300 });
+
+t3 = g.append("path")
+        .datum(triangolo3)
+        .attr('class', "triangolo" + i)
+        .attr("d", line)
+        .attr("visibility", "Hidden")
+        .attr("pointer-events", "fill")
+        .on("click", function(){
+           i = d3.select(this).attr("stroke-dashoffset");
+           changeAxes(2);
+        })
+
+var triangolo4 = [];
+triangolo4.push({ "x": 300, "y": 300 });
+point = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 3 / n_axis), 10)
+point1 = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 4 / n_axis), 10)
+point.x = point.x +4
+point.y = point.y -4
+point1.x = point1.x -4
+point1.y = point1.y +4
+triangolo4.push(point);
+triangolo4.push(point1);
+triangolo4.push({ "x": 300, "y": 300 });
+
+t4 = g.append("path")
+        .datum(triangolo4)
+        .attr('class', "triangolo" + i)
+        .attr("d", line)
+        .attr("visibility", "Hidden")
+        .attr("pointer-events", "fill")
+        .on("click", function(){
+           i = d3.select(this).attr("stroke-dashoffset");
+           changeAxes(3);
+        })
+
+var triangolo5 = [];
+triangolo5.push({ "x": 300, "y": 300 });
+point = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 4 / n_axis), 10)
+point1 = angleToCoordinate((Math.PI/2) + (2 * Math.PI * 5 / n_axis), 10)
+point.x = point.x +4
+point.y = point.y -4
+point1.x = point1.x +4
+point1.y = point1.y -4
+triangolo5.push(point);
+triangolo5.push(point1);
+triangolo5.push({ "x": 300, "y": 300 });
+
+t5 = g.append("path")
+        .datum(triangolo5)
+        .attr('class', "triangolo" + i)
+        .attr("d", line)
+        .attr("visibility", "Hidden")
+        .attr("pointer-events", "fill")
+        .on("click", function(){
+           i = d3.select(this).attr("stroke-dashoffset");
+           changeAxes(4);
+        })
 
 // reverse the axes
 function updateData(data, d){
@@ -167,8 +279,9 @@ function updateData(data, d){
 };
 
 // flip the axis
-function changeAxes(data, a, b){
+function changeAxes(a){
 
+    b = a+1;
     if(b==5) b=0;
 
     for(var i=0; i<data.length;i++){
